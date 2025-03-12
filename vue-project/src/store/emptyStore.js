404fe1779957ee89a78f59bd7e41e6cf07e3c529
Denys Bystryi <bystryydenis211999@gmail.com>
2025-03-12 19:44:37 +0200

    change api server for mockApi

--- vue-project/src/store/emptyStore.js
+++ vue-project/src/store/emptyStore.js
@@ -2,45 +2,53 @@ import { defineStore } from "pinia";
 import axios from "axios";
 import { nanoid } from "nanoid";
 export const useDataStore = defineStore("data", {
-    state: () => ({ data: [], selectedProject: null }),
-    actions: {
-        async getData() {
-            try {
-                const response = await axios.get("http://localhost:5000/projects");
-                console.log(response.data);
-                this.data = response.data;
-            }
-            catch (error) {
-                console.log("Помилка при запиті до Апі");
-            }
-        },
-        async deleteData(id) {
-            try {
-                await axios.delete(`http://localhost:5000/projects/${id}`);
-                this.data = this.data.filter((project) => project.id !== id);
-            }
-            catch (error) {
-                console.error("Помилка при видаленні:", error);
-            }
-        },
-        async addProject(Project) {
-            const projectWithId = { ...Project, id: nanoid() };
-            try {
-                const response = await axios.post("http://localhost:5000/projects", projectWithId);
-                this.data.push(response.data);
-            }
-            catch (error) {
-                console.error("Помилка при додаванні проекту:", error);
-            }
-        },
-        async editData(id, updatedData) {
-            try {
-                await axios.patch(`http://localhost:5000/projects/${id}`, updatedData);
-                await this.getData();
-            }
-            catch (error) {
-                console.error("Помилка редагування:", error);
-            }
-        },
+  state: () => ({ data: [], selectedProject: null }),
+  actions: {
+    async getData() {
+      try {
+        const response = await axios.get(
+          "https://67d1c24290e0670699bb72ac.mockapi.io/project"
+        );
+        console.log(response.data);
+        this.data = response.data;
+      } catch (error) {
+        console.log("Помилка при запиті до Апі");
+      }
     },
+    async deleteData(id) {
+      try {
+        await axios.delete(
+          `https://67d1c24290e0670699bb72ac.mockapi.io/project/${id}`
+        );
+        this.data = this.data.filter((project) => project.id !== id);
+      } catch (error) {
+        console.error("Помилка при видаленні:", error);
+      }
+    },
+    async addProject(Project) {
+      const projectWithId = { ...Project, id: nanoid() };
+      try {
+        const response = await axios.post(
+          "https://67d1c24290e0670699bb72ac.mockapi.io/project",
+          projectWithId
+        );
+        this.data.push(response.data);
+      } catch (error) {
+        console.error("Помилка при додаванні проекту:", error);
+      }
+    },
+    async editData(id, updatedData) {
+      try {
+        const { id: _, ...updatedDataWithoutId } = updatedData;
+
+        await axios.put(
+          `https://67d1c24290e0670699bb72ac.mockapi.io/project/${id}`,
+          updatedDataWithoutId
+        );
+        await this.getData();
+      } catch (error) {
+        console.error("Помилка редагування:", error);
+      }
+    },
+  },
 });
